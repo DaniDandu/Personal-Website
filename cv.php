ce49969c6c81cc18c43449ef83374d66fbd23fe6
DaniDandu <danidandu19@gmail.com>
2023-11-19 21:19:16 +0200

    creare si parcugerea unui tabel 2D

--- cv.php
+++ cv.php
@@ -10,65 +10,56 @@
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
-    <!-- <nav class="navbar navbar-expand-sm navbar-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="index.html">
-          <img title="Daniel Dandu" src="img/logo.png" style="width: 70px; height: 45px;">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      <div class="collapse navbar-collapse" id="collapsibleNavbar">
-        <ul class="navbar-nav">
-          <li class="nav-item text-center">
-            <a class="nav-link active" href="index.html">
-              <button type="button" class="btn" data-bs-toggle="popover">Home</button>
-            </a>
-          </li>
-          <li class="nav-item text-center">
-            <a class="nav-link" href="cv.html">
-              <button type="button" class="btn" data-bs-toggle="popover">CV</button>
-            </a>
-          </li>
-          <li class="nav-item text-center">
-            <a class="nav-link" href="hobby.html">
-              <button type="button" class="btn" data-bs-toggle="popover">Hobby</button>
-            </a>
-          </li>
-          <li class="nav-item text-center">
-            <a class="nav-link" href="contact.html">
-              <button type="button" class="btn" data-bs-toggle="popover">Contact</button>
-            </a>
-          </li>
-          <li class="nav-item">
-            <div class="ro-flag">
-              <a title="Română" class="nav-link" href="cv.html">
-                <button type="button" class="btn" data-bs-toggle="popover">
-                <img src="img/ro.png" style="height: 20px; width: 20px;">
-                </button>
-              </a>
-            </div>
-          </li>
-          <li class="nav-item"><div class="slash">/</div></li>
-          <li class="nav-item">
-            <div class="en-flag">
-              <a title="Engleză" class="nav-link" href="cv_en.html">
-                <button type="button" class="btn" data-bs-toggle="popover">
-                <img src="img/en.png" style="height: 20px; width: 20px;">
-                </button>
-              </a>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-    </nav> -->
-
+    
     <?php include 'header.php'?>
 
     <div class="container">
       <div class="row">
-        <div class="text col-md-6" style="text-align: left; padding-left: 5%;">
+        <div class="table col-md-6" style="text-align: left; padding-left: 5%;">
+          <h3>Educație</h3>
+          <table>
+            <tr>
+              <th>Facultatea</th>
+              <th>Instituția</th>
+              <th>Oraș</th>
+              <th>Diploma obținută</th>
+              <th>Anul inceperii</th>
+              <th>Anul absolvirii</th>
+            </tr>
+
+          <?php 
+            $listaEducatie = [
+              [
+                'facultatea' => 'ETcTi',
+                'institutia' => 'UPT',
+                'oras' => 'Timișoara',
+                'diploma_obtinuta' => 'Inginer',
+                'anul_inceperii' => 2019,
+                'anul_absolvirii' => 2023
+              ],
+              [
+                'facultatea' => 'ETcTi',
+                'institutia' => 'UPT',
+                'oras' => 'Timișoara',
+                'diploma_obtinuta' => 'Master',
+                'anul_inceperii' => 2023,
+                'anul_absolvirii' => 'prezent'
+              ]
+            ];
+            
+            foreach ($listaEducatie as $inregistrareEducatie) {
+              echo '<tr>';
+              foreach ($inregistrareEducatie as $element) {
+                echo '<td>'.$element.'</td>';
+              }
+              echo '<tr>';
+            }
+
+            echo '</table>';
+
+          ?>
+
+
           <p style="font-size: 20px; font-family:Verdana; margin: 13% 0 2% 0;"><b>Educație</b></p>
           <p style="font-size: 18px; font-family:Verdana; margin-bottom: 0.5%;">
             - <a id="link" href="https://www.etc.upt.ro/" target="_blank">Facultatea de Electronică, Telecomunicații și Tehnologii Informaționale</a>
@@ -92,7 +83,7 @@
           </p>
         </div>
         <div class="text col-md-6" style="text-align: left; padding-left: 10%;">
-          <p style="font-size: 20px; font-family:Verdana; margin: 13% 0 2% 0%;"><b>Aptitudini</b></p>
+          <!-- <p style="font-size: 20px; font-family:Verdana; margin: 13% 0 2% 0%;"><b>Aptitudini</b></p>
           <p style="font-size: 16px; font-family:Verdana; margin-bottom: 3%;">
              - Muncă în echipă <br>
              - Comunicare <br>
@@ -109,29 +100,12 @@
           <p style="font-size: 16px; font-family:Verdana;">
               - Română <br>
               - Engleză <br>
-          </p>
+          </p> -->
         </div>
       </div>
     </div>
 
     <?php include 'footer.php'?>
-
-    <!-- <footer class="footer py-3 border-top">
-      <div class="row">
-        <div class="copy-mobile col-md-6">© 2022 Copyright Daniel Dandu</div>
-        <div class="icons-mobile col-md-6">
-          <a href="mailto: danidandu19@gmail.com" target="_blank">
-            <i title="Send an email" class="fa fa-envelope-o fa-2x" aria-hidden="true"></i>
-          </a>
-          <a href="https://www.facebook.com/danny.dandu" target="_blank">
-            <i title="Facebook" class="fa fa-facebook-square fa-2x" aria-hidden="true"></i>
-          </a>
-          <a href="https://www.instagram.com/dani.dandu/" target="_blank">
-            <i title="Instagram" class="fa fa-instagram fa-2x" aria-hidden="true"></i>
-          </a>
-        </div>
-      </div>
-    </footer> -->
 
     <!-- Latest compiled JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
