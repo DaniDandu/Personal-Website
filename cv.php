f6633c881040916656896a5f3b6f1731314e70e8
DaniDandu <danidandu19@gmail.com>
2023-12-02 19:09:40 +0200

    editare si stergere

--- cv.php
+++ cv.php
@@ -15,55 +15,111 @@
       include 'config.php';
       include 'header.php';
     ?>
+    <?php
+      // UPDATE - primul pas: conectare la DB, tabela educatie si luam inregistrarea/randul cu id-ul transmis prin $_GET
+      
+      if (!empty($_GET['edit_id'])) {
+        echo "A fost apasat pe editare pentru inregistrarea cu id-ul ".$_GET['edit_id'];
+
+        $updateId = $_GET['edit_id'];
+        $sqlReadOnUpdate = "SELECT * FROM educatie WHERE id='$updateId'";
+        $result = mysqli_query($conn, $sqlReadOnUpdate) or die(mysql_error());
+
+        $row = mysqli_fetch_assoc($result);
+        
+        $id = $row['id'];
+        $facultatea = $row['facultatea'];
+        $institutia = $row['institutia'];
+        $diplomaObtinuta = $row['diploma_obtinuta'];
+        $anulInceperii = $row['anul_inceperii'];
+        $anulAbsovirii = $row['anul_absolvirii'];
+      }
+
+      // UPDATE - al doilea pas: daca s-a apasat butonul de actualizare => actualizam informatiile in DB pentru inregistrarea resp.
+      if (@$_POST['actualizeaza_info'] == 'Actualizeaza') {
+          
+        // echo 'S-a apasat pe butonul de actualizare';
+
+        $id = $_POST['id'];
+        $facultatea = $_POST['facultatea'];
+        $institutia = $_POST['institutia'];
+        $diplomaObtinuta = $_POST['diploma_obtinuta'];
+        $anulInceperii = $_POST['anul_inceperii'];
+        $anulAbsovirii = $_POST['anul_absolvirii'];
+
+        $sqlUpdate = "UPDATE educatie SET
+                        facultatea = '$facultatea',
+                        institutia = '$institutia',
+                        diploma_obtinuta = '$diplomaObtinuta',
+                        anul_inceperii = '$anulInceperii',
+                        anul_absolvirii = '$anulAbsovirii'
+                      WHERE id='$id'";
+        
+        $result = mysqli_query($conn, $sqlUpdate) or die(mysql_error());
+      }
+    ?>
 
     <div class="container">
       <div class="row">
         <div class="col-md-6" style="text-align: left; padding-left: 5%;">
           <h3 style="text-align: left;">Educație</h3>
           <form action="<?=$_SERVER['PHP_SELF'];?>" method="POST">
+            <input type="hidden" name="id" value="<?=@$id?>">
+
             <div class="mb-3">
               <label for="facultatea" class="form-label">Facultatea:*</label>
-              <input type="text" name="facultatea" class="form-control" required>
+              <input type="text" name="facultatea" class="form-control" value="<?php echo @$facultatea?>" required>
             </div>
+
             <div class="mb-3">
               <label for="institutia" class="form-label">Instituția:*</label>
-              <input type="text" name="institutia" class="form-control" required>
+              <input type="text" name="institutia" class="form-control" value="<?=@$institutia?>" required>
             </div>
-            <div class="mb-3">
-              <label for="oras" class="form-label">Oraș:</label>
-              <input type="text" name="oras" class="form-control">
-            </div>
+
             <div class="mb-3">
               <label for="diploma_obtinuta" class="form-label">Diploma obținută:*</label>
-              <input type="text" name="diploma_obtinuta" class="form-control" required>
+              <input type="text" name="diploma_obtinuta" class="form-control" value="<?=@$diplomaObtinuta?>" required>
             </div>
+
             <div class="mb-3">
               <label for="anul_inceperii" class="form-label">Anul inceperii:*</label>
-              <input type="datetime" name="anul_inceperii" class="form-control" required>
+              <input type="datetime" name="anul_inceperii" class="form-control" value="<?=@$anulInceperii?>" required>
             </div>
+
             <div class="mb-3">
               <label for="anul_absolvirii" class="form-label">Anul absolvirii:*</label>
-              <input type="datetime" name="anul_absolvirii" class="form-control" required>
+              <input type="datetime" name="anul_absolvirii" class="form-control" value="<?=@$anulAbsovirii?>" required>
             </div>
-            <button type="submit" value="Educație înregistrată" name="salveaza_info" class="btn btn-default">Înregistrează</button>
+
+            <input type="submit" value="Trimite" name="salveaza_info">
+            <input type="submit" value="Actualizeaza" name="actualizeaza_info">
           </form>
 
           <?php
-            if (@$_POST['salveaza_info'] == 'Educație înregistrată') {
+            if (@$_POST['salveaza_info'] == 'Trimite') {
               $facultatea = $_POST['facultatea'];
               $institutia = $_POST['institutia'];
-              $oras = $_POST['oras'];
               $diplomaObtinuta = $_POST['diploma_obtinuta'];
               $anulInceperii = $_POST['anul_inceperii'];
               $anulAbsovirii = $_POST['anul_absolvirii'];
 
               
-              $sqlInsert = "INSERT INTO educatie (facultatea, institutia, oras, diploma_obtinuta, anul_inceperii, anul_absolvirii)
-                            VALUES ('$facultatea', '$institutia', '$oras', '$diplomaObtinuta', '$anulInceperii', '$anulAbsovirii');";
+              $sqlInsert = "INSERT INTO educatie (facultatea, institutia, diploma_obtinuta, anul_inceperii, anul_absolvirii)
+                            VALUES ('$facultatea', '$institutia', '$diplomaObtinuta', '$anulInceperii', '$anulAbsovirii');";
               // echo "Instructiune inserare: ".$sqlInsert;
               if(!mysqli_query($conn, $sqlInsert)) {
               echo "<br>Nu a functionat"; echo mysqli_error ( $conn );
               }
+            }
+
+            if (!empty($_GET['delete_id'])) {
+              echo "S-a sters inregistrarea cu id-ul ".$_GET['delete_id'];
+
+              $deleteId = $_GET['delete_id'];
+
+              $sqlDelete = "DELETE FROM educatie WHERE id='$deleteId'";
+              $result = mysqli_query($conn, $sqlDelete) or die(mysql_error());
+
             }
           ?>
 
@@ -74,7 +130,6 @@
             <tr>
               <th>Facultatea</th>
               <th>Instituția</th>
-              <th>Oraș</th>
               <th>Diploma obținută</th>
               <th>Anul inceperii</th>
               <th>Anul absolvirii</th>
@@ -91,10 +146,11 @@
               echo '<tr>';
               echo '<td>'.$row['facultatea'].'</td>';
               echo '<td>'.$row['institutia'].'</td>';
-              echo '<td>'.$row['oras'].'</td>';
               echo '<td>'.$row['diploma_obtinuta'].'</td>';
               echo '<td>'.$row['anul_inceperii'].'</td>';
               echo '<td>'.$row['anul_absolvirii'].'</td>';
+              echo '<td><a href="cv.php?edit_id='.$row['id'].'"><i class="fa-solid fa-pen-to-square"></i></a></td>';
+              echo '<td><a href="cv.php?delete_id='.$row['id'].'"><i class="fa-solid fa-trash"></i></a></td>';
               echo '</tr>';
             }
 
