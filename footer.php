535ee418d6ff7fceb2eafef0b3d607a3f3fbafaa
DaniDandu <danidandu19@gmail.com>
2023-12-17 00:18:07 +0200

    some changes

--- footer.php
+++ footer.php
@@ -12,21 +12,21 @@
 <body>
     
 <footer class="footer py-3 border-top">
-      <div class="row">
-        <div class="copy-mobile col-md-6" style="padding-left: 2%;">© 2022 Copyright Daniel Dandu</div>
-        <div class="icons-mobile col-md-6">
-          <a href="mailto: danidandu19@gmail.com" target="_blank">
-            <i title="Send an email" class="fa fa-envelope-o fa-2x" aria-hidden="true"></i>
-          </a>
-          <a href="https://www.facebook.com/danny.dandu" target="_blank">
-            <i title="Facebook" class="fa fa-facebook-square fa-2x" aria-hidden="true"></i>
-          </a>
-          <a href="https://www.instagram.com/dani.dandu/" target="_blank">
-            <i title="Instagram" class="fa fa-instagram fa-2x" aria-hidden="true"></i>
-          </a>
-        </div>
-      </div>
-    </footer>
+  <div class="row" style="width: 99%;">
+    <div class="copy-mobile col-md-6" style="padding-left: 4%;">© 2022 Copyright Daniel Dandu</div>
+    <div class="icons-mobile col-md-6">
+      <a href="mailto: danidandu19@gmail.com" target="_blank">
+        <i title="Send an email" class="fa fa-envelope-o fa-2x" aria-hidden="true"></i>
+      </a>
+      <a href="https://www.facebook.com/danny.dandu" target="_blank">
+        <i title="Facebook" class="fa fa-facebook-square fa-2x" aria-hidden="true"></i>
+      </a>
+      <a href="https://www.instagram.com/dani.dandu/" target="_blank">
+        <i title="Instagram" class="fa fa-instagram fa-2x" aria-hidden="true"></i>
+      </a>
+    </div>
+  </div>
+</footer>
 
 </body>
 </html>
