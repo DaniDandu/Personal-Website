ba57c7a0596ef43308b7d431a1a2188620bb6466
DaniDandu <danidandu19@gmail.com>
2023-12-23 02:36:50 +0200

    changed copyright

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
     
 <footer class="footer py-3 border-top">
   <div class="row" style="width: 99%;">
-    <div class="copy-mobile col-md-6" style="padding-left: 4%;">© 2022 Copyright Daniel Dandu</div>
+    <div class="copy-mobile col-md-6" style="padding-left: 4%;">© 2023 Copyright Daniel Dandu</div>
     <div class="icons-mobile col-md-6">
       <a href="mailto: danidandu19@gmail.com" target="_blank">
         <i title="Send an email" class="fa fa-envelope-o fa-2x" aria-hidden="true"></i>
